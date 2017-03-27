97df5b2b08a2f3100dc0d2a2621ceb3590f66c78
Anri <anridig@gmail.com>
2017-03-27 15:26:57 +0700

    Txns.

--- reports/create-transactions-report.js
+++ reports/create-transactions-report.js
@@ -14,6 +14,7 @@ function createTxnEntry (ws, t) {
     workspaceOwner: ws.workspaceOwner,
     workspaceEmail: ws.workspaceEmail,
     billingPeriodStartDate: t.billingPeriodStartDate,
+    currentBillingCycle: t.currentBillingCycle || 0,
     amount: t.amount ? parseInt(t.amount, 10) : 0,
     plan: t.planId
   }
@@ -33,18 +34,18 @@ function renderTaskworldReport (transactionsJsonFile) {
         return
       }
 
-      const month = date.format('YYYY-MM')
-      if (month === 'Invalid date') {
+      const past = date.format('YYYY-MM')
+      if (past === 'Invalid date') {
         console.log('Invalid date for transaction:', y)
       }
 
       const amount = y.amount ? parseInt(y.amount, 10) : 0
 
-      if (!acc.month.items[month]) {
-        acc.month.items[month] = []
+      if (!acc.past.items[past]) {
+        acc.past.items[past] = []
       }
-      acc.month.items[month].push(createTxnEntry(x, y))
-      acc.month.stats.total += amount
+      acc.past.items[past].push(createTxnEntry(x, y))
+      acc.past.stats.total += amount
 
       if (y.nextBillingDate) {
         const futureDay = Moment(y.nextBillingDate, 'YYYY-MM-DD')
@@ -62,7 +63,7 @@ function renderTaskworldReport (transactionsJsonFile) {
     })
     return acc
   }, {
-    month: {
+    past: {
       stats: { total: 0 },
       items: { }
     },
@@ -72,21 +73,40 @@ function renderTaskworldReport (transactionsJsonFile) {
     }
   })
 
-  const printMonthReport = (title, map) => {
-    console.log('Report:', title)
-    let months = Object.keys(map.items)
-    months.sort()
-    months.forEach(month => {
-      const items = map.items[month]
-      const workspaceCount = (new Set(items.map(x => x.workspaceId))).size
-      const totalAmount = items.reduce((acc, x) => acc + x.amount, 0)
-      console.log(`${month}: ${workspaceCount} workspaces, amount: $${totalAmount.toLocaleString()}`)
+  const print = map => {
+    let dates = Object.keys(map.items)
+    dates.sort()
+    dates.forEach((date, i) => {
+      const workspaceIds = new Set(map.items[date].map(x => x.workspaceId))
+      const totals = map.items[date].reduce((acc, x) => {
+        const isRecurring = x.currentBillingCycle > 1
+        if (isRecurring) {
+          acc.recurring += x.amount
+        } else {
+          acc.new += x.amount
+        }
+        return acc
+      }, {
+        new: 0,
+        recurring: 0
+      })
+
+      const totalForDate = totals.new + totals.recurring
+      console.log(
+        `${date}: ${pad(workspaceIds.size)} workspaces, ` +
+        `amounts:` +
+        `${pad('$' + totals.new.toLocaleString(), 10)} new` +
+        `${pad('$' + totals.recurring.toLocaleString(), 10)} recur - ` +
+        `${pad('$' + totalForDate.toLocaleString(), 10)}`
+      )
     })
-    console.log(`Total recurring: $${map.stats.total.toLocaleString()}`)
   }
 
-  printMonthReport('Future Payments', report.future)
-  printMonthReport('Past Payments', report.month)
+  console.log('Past Payments:')
+  print(report.past)
+
+  console.log('Future Payments:')
+  print(report.future)
 
   // let html = Fs.readFileSync(Path.join(__dirname, 'layout.html'), 'utf8')
   //
@@ -103,4 +123,11 @@ function renderTaskworldReport (transactionsJsonFile) {
   //     console.log('res=', res)
   //   })
   // }
+}
+
+function pad (str, size = 6) {
+  const s = str.toString()
+  const len = s.length
+  const pad = ' '.repeat(size - len < 0 ? 0 : size - len)
+  return pad + s
 }
